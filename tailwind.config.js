0cafecd2d1fc72daf041c70b61e63ecbf1fb54b4
santa116-1 <totoplife116@gmail.com>
2024-09-03 14:39:24 +0900

    add/about-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     './*.html',             // For HTML files in the root directory
-    './src/**/*.{js,jsx}',  // For JS and JSX files in the src directory
+    './assets/**/*.{js}', 
+    "./assets/**/*.css",  // For JS and JSX files in the src directory
   ],
   theme: {
     extend: {},
