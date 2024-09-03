03401709d88d686558f62cef7d7198ab663decc1
santa116-1 <totoplife116@gmail.com>
2024-09-03 14:17:47 +0900

    add/about-page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './*.html',             // For HTML files in the root directory
+    './src/**/*.{js,jsx}',  // For JS and JSX files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
